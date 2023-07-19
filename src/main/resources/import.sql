a84381776a9d56149474708cc0453fc0d1c29788
Halan Caio <hallankayo20@gmail.com>
2023-07-19 13:33:09 -0300

    create project, configure archives & User entity

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user (name, email, phone, password) values ('halan', 'hallan@gmail.com', '999999999', '123456')
+INSERT INTO tb_user (name, email, phone, password) values ('kayo', 'kayo@gmail.com', '888888888', '654321')
+INSERT INTO tb_user (name, email, phone, password) values ('pereira', 'pereira@gmail.com', '777777777', '6454354')
+INSERT INTO tb_user (name, email, phone, password) values ('nascimento', 'nascimento@gmail.com', '666666666', '5435352')

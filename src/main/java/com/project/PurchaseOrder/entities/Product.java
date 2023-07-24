b9f5915ab9c9d8f096bedbcc0bef29f179e8cfca
Halan Caio <hallankayo20@gmail.com>
2023-07-24 14:07:01 -0300

    Many-to-Many association with JoinTable

--- src/main/java/com/project/PurchaseOrder/entities/Product.java
+++ src/main/java/com/project/PurchaseOrder/entities/Product.java
@@ -4,12 +4,17 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -22,8 +27,9 @@ public class Product {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
@@ -31,7 +37,6 @@ public class Product {
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -100,7 +105,4 @@ public class Product {
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
-	
 }

9e4a63bab58bfa546e6dab9f2af892557b09952f
Halan Caio <hallankayo20@gmail.com>
2023-07-26 18:31:48 -0300

    user update

--- src/main/java/com/project/PurchaseOrder/controllers/UserController.java
+++ src/main/java/com/project/PurchaseOrder/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,11 @@ public class UserController {
 	@DeleteMapping(value = "/{id}")
 	public void deleteUser(@PathVariable Long id) {
 		userService.deleteUser(id);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public User updateUser(@PathVariable Long id, @RequestBody User user) {
+		return userService.updateUser(id, user);
 	}
 	
 }
--- src/main/java/com/project/PurchaseOrder/services/UserService.java
+++ src/main/java/com/project/PurchaseOrder/services/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.project.PurchaseOrder.entities.User;
 import com.project.PurchaseOrder.repositories.UserRepository;
@@ -37,5 +40,11 @@ public class UserService {
 	
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
+	}
+	
+	public User updateUser(Long id, User user) {
+		User result = userRepository.getReferenceById(id);
+		BeanUtils.copyProperties(user, result, "id");
+		return userRepository.save(result);
 	}
 }

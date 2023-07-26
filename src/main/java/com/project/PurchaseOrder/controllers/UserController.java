72dfef03a8c4fceac159cb993560b6219429e29d
Halan Caio <hallankayo20@gmail.com>
2023-07-26 18:23:20 -0300

    user delete

--- src/main/java/com/project/PurchaseOrder/controllers/UserController.java
+++ src/main/java/com/project/PurchaseOrder/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,9 @@ public class UserController {
 		return ResponseEntity.created(uri).body(user);
 	}
 	
-	
+	@DeleteMapping(value = "/{id}")
+	public void deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+	}
 	
 }
